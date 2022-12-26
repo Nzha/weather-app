25d21d1f908fc920c809ae05c7460dc126373f03
Adrien <adrien.biz@gmail.com>
2022-12-26 15:10:57 +0400

    Fetch API using async/await

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+
+const getWeather = async function getWeatherFromAPI() {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=2c90294ffc8f3aba96a28d8de4977cd3`, {mode: 'cors'})
+    const weather = await response.json();
+    console.log(weather);
+}
+
+getWeather();
