a3836e86afac40b5a55f6e849eb76b3f367a3f97
Adrien <adrien.biz@gmail.com>
2022-12-27 22:03:02 +0400

    Fetch weather data and process it

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 import './style.css';
 
-const getWeather = async function getWeatherFromAPI() {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=2c90294ffc8f3aba96a28d8de4977cd3`, {mode: 'cors'})
-    const weather = await response.json();
-    console.log(weather);
+let weather = {};
+
+const getWeather = async function getCurrentWeatherFromAPI() {
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Dubai&limit=5&appid=2c90294ffc8f3aba96a28d8de4977cd3`, {mode: 'cors'});
+    const geocode = await response.json();
+    const lat = geocode[0].lat;
+    const lon = geocode[0].lon;
+    console.log(geocode);
+
+    weather.name = geocode[0].name;
+
+    const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2c90294ffc8f3aba96a28d8de4977cd3&units=metric`, {mode: 'cors'});
+    const weatherData = await weatherResponse.json();
+    console.log(weatherData);
+
+    return weatherData;
 }
 
-getWeather();
+const saveData = function processWeatherDataFromAPI(Data) {
+    // City name has already been saved
+    weather.country = Data.sys.country;
+    weather.temp = Data.main.temp;
+    weather.feelslike = Data.main.feels_like;
+    weather.main = Data.weather[0].main;
+    weather.description = Data.weather[0].description;
+
+    return weather;
+}
+
+const getAndSaveData = async function getAndSaveWeatherData() {
+    const getWeatherData = await getWeather();
+    const saveWeatherData = await saveData(getWeatherData);
+
+    console.log(saveWeatherData);
+    return saveWeatherData;
+}
+
+getAndSaveData();
