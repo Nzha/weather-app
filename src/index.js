9a6338d0323e608c8bfa62767658310d5f80d468
Adrien <adrien.biz@gmail.com>
2022-12-28 14:53:29 +0400

    Feat: user can search current weather

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import './style.css';
 import { default as getAndSaveWeatherData } from './weatherData';
 
-getAndSaveWeatherData('Dubai');
+const searchBtn = document.querySelector('.search-btn');
+const search = document.querySelector('#search')
+
+const loadData = function loadWeatherData() {
+    getAndSaveWeatherData(search.value);
+}
+
+searchBtn.addEventListener('click', loadData);
+
+// Let user press enter to run search
+search.addEventListener('keydown', function(e) {
+    if (e.keyCode === 13) getAndSaveWeatherData(search.value);
+});
